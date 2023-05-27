3cdb91e0600a3d7d899ffda2e8d5c011dd89aab6
Daniella Manuel <dmanuel@highlysucceed.com>
2023-05-27 16:51:58 +0800

    Created a factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Faker\Factory as FakerFactory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'product_name' => $this->faker->text(20),
+            'product_description' => $this->faker->text(200),
+            'product_price' => $this->faker->randomFloat(2, 0, 100000)
+        ];
+    }
+}
