59e267d5c8c3393c05ca16b5adcefd6ef993ff9b
Daniella Manuel <daniellamnl.dev@gmail.com>
2023-05-30 17:36:59 +0800

    Cache response of the Product API

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\ProductResource;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
@@ -31,20 +32,37 @@ class ProductController extends Controller
             $add_product = new Product($product_data);
             $add_product->save();
 
-            return [
+            //Cache new product
+            $cache_key = 'product_' . $add_product->id;
+            Cache::put($cache_key, $add_product);
+
+            return response()->json([
                 'message' => 'Product added successfully.',
                 'product' => $add_product,
-            ];
+            ]);
         }
     }
 
     public function show(Product $product)
     {
+        //Check if cache exists
+        $cache_key = 'product_' . $product->id;
+
+        if (Cache::has($cache_key)) {
+            Cache::get($cache_key);
+        } else {
+            Cache::put($cache_key, $product);
+        }
+        
         return new ProductResource($product);
     }
 
     public function update(Request $request, Product $product)
     {
+        //Remove the previous cache (if there's any)
+        $cache_key = 'product_' . $product->id;
+        Cache::forget($cache_key);
+
         $validate = Validator::make($request->all(), [
             'product_name' => 'required|max:255',
             'product_description' => 'required',
@@ -59,15 +77,22 @@ class ProductController extends Controller
             $update_product->fill($product_data);
             $update_product->save();
 
-            return [
+            //Add new cache
+            Cache::put($cache_key, $product);
+
+            return response()->json([
                 'message' => 'Product updated successfully.',
                 'product' => $product,
-            ];
+            ]);
         } 
     }
 
     public function destroy(Product $product)
     {
+        //Remove cache (if there's any)
+        $cache_key = 'product_' . $product->id;
+        Cache::forget($cache_key);
+
         $delete_product = Product::where('id', $product->id)->firstOrFail();
         $delete_product->delete();
 
